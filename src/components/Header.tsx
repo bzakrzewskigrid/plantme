42058113cd07164ad65577a890435ac63fced6bb
bzakrzewski <bzakrzewski@griddynamics.com>
2024-09-09 13:01:26 +0200

    init

---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+export const Header = () => {
+  return (
+    <header>
+      <h1 className="text-5xl font-bold text-primary-green">Lorem ipsum dolor sit amet</h1>
+    </header>
+  );
+};
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        outfit: ['Outfit', 'sans-serif'],
+      },
+    },
+    colors: {
+      'primary-green': '#3A6150',
+      'placeholder-green': '#3A6150B2',
+      black: '#000000',
+      'primary-black': '#111111',
+      'label-back': '#131516',
+      white: '#FFFFFF',
+      'primary-grey': '#00000099',
+      'primary-light-grey': '#11111180',
+      'footer-light-grey': '#E9E9E9',
+    },
+  },
+  plugins: [],
+};
